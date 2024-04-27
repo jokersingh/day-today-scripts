1c93c8d504f8fd61c88aef56b7d73a8c702f4b4a
Bibhuti Barun <sakhibarun@gmail.com>
2024-04-26 19:50:14 -0500

    Update AESExample.java

--- AESExample.java
+++ AESExample.java
@@ -8,7 +8,6 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Arrays;
 import java.util.Base64;
 
 public class AESExample {
@@ -17,20 +16,15 @@ public class AESExample {
         try {
             String originalString = "Hello, World! This is a secret message.";
             String password = "MySecretPassword";
-
-            // Generate a random key and IV
-
-            byte[] publicKey = generatePublicKey(password);
-            //byte[] salt = joinByteArray(key, publicKey);
-
+            AESExample aesExample = new AESExample();
             // Encrypt the string
-            String encryptedString = encrypt(originalString, password);
+            String encryptedString = aesExample.encrypt(originalString, password);
             //1Yx33+GkktzXacq5iUq9Qbtvg4SN1fBNujrSqjG+DFXyarrb7EUk/bpz3PYykGOI
 
             System.out.println("Encrypted string: " + encryptedString);
 
             // Decrypt the encrypted string
-            String decryptedString = decrypt(encryptedString, password);
+            String decryptedString = aesExample.decrypt(encryptedString, password);
 
             System.out.println("Decrypted string: " + decryptedString);
 
@@ -39,72 +33,77 @@ public class AESExample {
         }
     }
 
-    public static byte[] hashPassword(String password, byte[] salt) {
+    public String encrypt(String strToEncrypt, String password) {
         try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            digest.reset();
-            digest.update(salt);
-            byte[] hashedPassword = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-            return hashedPassword;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return null;
+            // Generate a random key and IV
+            byte[] key = generateRandomKey();
+            byte[] iv = generateRandomIV();
+            // Hash Key using key and IV
+            byte[] hashedKey = hashPassword(password, key);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(hashedKey, "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+            //Encrypted text
+            byte[] encryptedBytes = cipher.doFinal(strToEncrypt.getBytes());
+            //Adding Key, IV and Text
+            byte[] finalEncrypted = joinByteArray(key, iv, encryptedBytes);
+            return Base64.getEncoder().encodeToString(finalEncrypted);
+        }catch (Exception e){
+            //Log Exception
         }
+
+        return "";
     }
 
-    public static String encrypt(String strToEncrypt, String password) throws Exception {
-        byte[] key = generateRandomKey();
-        byte[] iv = generateRandomIV();
-        byte[] hashedKey = hashPassword(password, key);
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        SecretKeySpec secretKeySpec = new SecretKeySpec(hashedKey, "AES");
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
-        byte[] encryptedBytes = cipher.doFinal(strToEncrypt.getBytes());
-        byte[] finalEncrypted = joinByteArray(key, iv, encryptedBytes);
-        return Base64.getEncoder().encodeToString(finalEncrypted);
+    public String decrypt(String strToDecrypt, String password) {
+        try {
+            //Decrypting text
+            byte[] finalBytes = Base64.getDecoder().decode(strToDecrypt);
+            //Breaking text to find key, IV and Actual Encrypted Text
+            ByteBuffer bb = ByteBuffer.wrap(finalBytes);
+            byte[] key = new byte[16];
+            byte[] nonce = new byte[16];
+            byte[] actualText = new byte[finalBytes.length - 32];
+            bb.get(key, 0, key.length);
+            bb.get(nonce, 0, nonce.length);
+            bb.get(actualText, 0, actualText.length);
+            //Generating Hash Key using public key and salt
+            byte[] hashedKey = hashPassword(password, key);
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(hashedKey, "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(nonce);
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+            byte[] decryptedBytes = cipher.doFinal(actualText);
+            return new String(decryptedBytes);
+        }catch (Exception e){
+            //Log Exception
+        }
+
+        return "";
     }
 
-
-
-    public static String decrypt(String strToDecrypt, String password) throws Exception {
-        byte[] finalBytes = Base64.getDecoder().decode(strToDecrypt);
-        ByteBuffer bb = ByteBuffer.wrap(finalBytes);
-        byte[] salt = new byte[16];
-        byte[] nonce = new byte[16];
-        byte[] actualText = new byte[finalBytes.length-32];
-        bb.get(salt, 0, salt.length);
-        bb.get(nonce, 0, nonce.length);
-        bb.get(actualText, 0, actualText.length);
-        byte[] hashedKey = hashPassword(password, salt);
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        SecretKeySpec secretKeySpec = new SecretKeySpec(hashedKey, "AES");
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(nonce);
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
-        byte[] decryptedBytes = cipher.doFinal(actualText);
-        return new String(decryptedBytes);
+    private static byte[] hashPassword(String password, byte[] salt) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.reset();
+        digest.update(salt);
+        return digest.digest(password.getBytes(StandardCharsets.UTF_8));
     }
 
-
-    public static byte[] generatePublicKey(String publicKey) {
-        byte[] bytes = publicKey.getBytes();
-        byte[] key = Arrays.copyOf(bytes, 16);
-        return key;
-    }
-
-    public static byte[] generateRandomKey() {
+    private static byte[] generateRandomKey() {
         byte[] key = new byte[16];
         new SecureRandom().nextBytes(key);
         return key;
     }
 
-    public static byte[] generateRandomIV() {
+    private static byte[] generateRandomIV() {
         byte[] iv = new byte[16];
         new SecureRandom().nextBytes(iv);
         return iv;
     }
 
-    public static byte[] joinByteArray(byte[] byte1, byte[] byte2, byte[] byte3) {
+    private static byte[] joinByteArray(byte[] byte1, byte[] byte2, byte[] byte3) {
 
         return ByteBuffer.allocate(byte1.length + byte2.length+byte3.length)
                 .put(byte1)
